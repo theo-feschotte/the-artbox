38dbc0dd911386e0d634dcc66d4193b088070c35
Théo Feschotte <theofeschotte@gmail.com>
2023-11-11 14:24:05 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <header>
-    <a href="<?php echo $siteURL ?>" target="_self"><img id="logo" src="<?php echo $siteLOGO_src ?>" alt="<?php echo $siteLOGO_alt ?>"></a>
+    <a href="<?php echo($siteURL) ?>" target="_self"><img id="logo" src="<?php echo($siteLOGO_src) ?>" alt="<?php echo($siteLOGO_alt) ?>"></a>
     <nav>
         <ul>
-            <li><a href="<?php echo $siteURL ?>" target="_self">Accueil</a></li>
+            <li><a href="<?php echo($siteURL) ?>" target="_self">Accueil</a></li>
         </ul>
     </nav>
 </header>
