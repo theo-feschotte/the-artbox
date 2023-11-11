8b6d873f08f4d6603b4a01686b66f42e8f3ceebe
Théo Feschotte <theofeschotte@gmail.com>
2023-11-11 14:04:58 +0100

    Dynamisation des liens back-to-home

--- functions.php
+++ functions.php
@@ -1,5 +1,13 @@
 <?php
 
+// Site URL - index.php
+$siteURL = sprintf(
+    "%s://%s%s",
+    isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
+    $_SERVER['SERVER_NAME'],
+    $_SERVER['REQUEST_URI'],
+);
+
 // $oeuvres = [
 //     "foo" => "bar",
 //     "multi" => [
--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <header>
-    <a href="index.php" target="_self"><img id="logo" src="img/logo.png" alt="Logo Artbox"></a>
+    <a href="<?php echo $siteURL ?>" target="_self"><img id="logo" src="img/logo.png" alt="Logo Artbox"></a>
     <nav>
         <ul>
-            <li><a href="index.php" target="_self">Accueil</a></li>
+            <li><a href="<?php echo $siteURL ?>" target="_self">Accueil</a></li>
         </ul>
     </nav>
 </header>
